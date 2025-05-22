28e7d2b08ac4d787a41e1ab9c4a85f89bf553b88
highhunter777 <2090127048@qq.com>
2025-05-22 10:56:23 +0800

    尝试第一次上传

---
+++ Assets/script/menuScript/BGMScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMScript : MonoBehaviour
+{
+    public static BGMScript BGM;  // 单例实例
+    private AudioSource audioSource;    // 音频源组件
+
+    void Awake()
+    {
+        // 如果 Instance 为 null，则设置它为当前的 BGMScript 实例，否则销毁当前物体
+        if (BGM == null)
+        {
+            BGM = this;
+            DontDestroyOnLoad(this.gameObject);  // 确保这个物体在场景切换时不销毁
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 获取音频源组件
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/script/settingUIScript/returnScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class returnScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void settingUI()
+    {
+        GameObject gameSettingUI = GameObject.FindGameObjectWithTag("gameSettingUI").transform.GetChild(0).gameObject;
+        gameSettingUI.SetActive(false);
+        GameObject menu = GameObject.FindGameObjectWithTag("menu").transform.GetChild(0).gameObject;
+        menu.SetActive(true);
+    }
+}
